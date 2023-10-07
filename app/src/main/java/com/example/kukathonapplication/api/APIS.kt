ca13e27b2a704986c2371201fed4d4729171fa78
cityshin <ssk7750@naver.com>
2023-10-08 03:57:33 +0900

    Feat. 기업 정보 api 추기

--- app/src/main/java/com/example/kukathonapplication/api/APIS.kt
+++ app/src/main/java/com/example/kukathonapplication/api/APIS.kt
@@ -1,7 +1,15 @@
 package com.example.kukathonapplication.api
 
+import com.example.kukathonapplication.model.ResponseEnterpriseList
+import com.example.kukathonapplication.model.enterpriseInfo
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface APIS {
+
+    @GET("enterprise/list")
+    fun getEnterpriseList(
+
+    ) : Call<ResponseEnterpriseList>
 
 }
