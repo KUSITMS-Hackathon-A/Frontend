4525bbc3d30822e8883df8f9ea1cd0064287533c
cityshin <ssk7750@naver.com>
2023-10-07 18:31:14 +0900

    Feat. 가치 보도 page1 추가

--- app/src/main/java/com/example/kukathonapplication/view/TwoFragment.kt
+++ app/src/main/java/com/example/kukathonapplication/view/TwoFragment.kt
@@ -1,6 +1,9 @@
 package com.example.kukathonapplication.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -27,6 +30,11 @@ class TwoFragment : BaseFragment<FragmentTwoBinding>(R.layout.fragment_two) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+//        binding.news.setOnClickListener {
+//            val news = "https://www.rebud.co.kr/News/?q=YToxOntzOjEyOiJrZXl3b3JkX3R5cGUiO3M6MzoiYWxsIjt9&bmode=view&idx=7075162&t=board"
+//            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(news))
+//            startActivity(intent)
+//        }
 
 //        val webSettings: WebSettings = binding.web.settings
 //        webSettings.javaScriptEnabled = true
